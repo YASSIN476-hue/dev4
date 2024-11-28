254b3d32f126786c962abd2031d8a16d522ad040
Yassine <qassiyassine1@gmail.com>
2024-11-28 13:25:59 +0100

    projetcode

---
+++ pages/Sidebar.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="../css/style.css">
+  <link rel="stylesheet" href="../css/full.min..css">
+</head>
+
+<body>
+
+  <div class="drawer lg:drawer-open h-screen">
+    <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-side">
+      <ul class="menu bg-base-200 text-base-content h-screen w-80 p-4">
+        <!-- Sidebar content here -->
+        <li><a>Sidebar Item 1</a></li>
+        <li><a>Sidebar Item 2</a></li>
+      </ul>
+    </div>
+  </div>
+</body>
+
+</html>
